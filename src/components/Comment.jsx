78d10e90aebde85b36fdacb470d0307ee2c0cba7
andre-gicoski <andregicoskisz@gmail.com>
2025-03-28 11:12:56 -0300

    feat: Implement deleteComment, add value key in arrays

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,31 +1,37 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css';
-import { Avatar } from './Avatar';
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
 
-export function Comment({ content }) {
-    return (
-        <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/andre-gicoski.png"/>
-            <div className={styles.commentBox}>
-                <div className={styles.commentContent}>
-                    <header>
-                        <div className={styles.authorAndTime}>
-                            <strong>André Gicoski</strong>
-                            <time title="26 de março as 16:30" dateTime="2025-03-26 16:23:30">Cerca de 1h atrás</time>
-                        </div>
-                        <button title='Deletar comentário'>
-                            <Trash size={24} />
-                        </button>
-                    </header>
-                    <p>{content}</p>
-                </div>
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+}
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/andre-gicoski.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>André Gicoski</strong>
+              <time title="26 de março as 16:30" dateTime="2025-03-26 16:23:30">
+                Cerca de 1h atrás
+              </time>
             </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
         </div>
-    )
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
 }
